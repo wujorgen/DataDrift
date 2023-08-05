3db60c20a1ac77a40e1ae712e1f6dbd6d1bc16ab
wujorgen <wujorgen@gmail.com>
2023-08-05 15:03:50 -0400

    hmm

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -2,7 +2,7 @@ from BLUESKY.genurls import gen_zipcodes, gen_cars_com_urls
 
 
 def test_zipcodes():
-    assert gen_zipcodes(zarg=False) == [15238, 47907]
+    assert gen_zipcodes(zarg=False) == [15238, 47907, 78701, 95814]
 
 def test_url_generator():
     print(gen_cars_com_urls(["Ford"],["Mustang"]))
